d9a5bb109089ed905f8a4816c54132ba8e2f92c9
StaTov <tovchfamily@gmail.com>
2024-02-25 14:53:52 +0300

    chore(jest.config.ts): add coverageThreshold

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type {Config} from 'jest';
 const config: Config = {
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!**/node_modules/**',
@@ -12,6 +13,15 @@ const config: Config = {
     '!**/types.{ts, tsx}',
     '!src/index.tsx'
   ],
+  // TODO раскомментировать для отслеживания процента покрытия.
+  //   coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: -2,
+  //   },
+  // },
   preset: "ts-jest",
   testEnvironment: "jsdom",
   "modulePaths": [
